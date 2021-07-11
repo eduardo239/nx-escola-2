f09cfd35195c070c4293a35310452579b913db65
eduardo239 <eucrieiumaconta@gmail.com>
2021-07-11 16:29:12 -0300

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -4,10 +4,10 @@
 
 const Layout = ({ children }) => {
   return (
-    <div className={s.container}>
+    <div>
       Nav
-      <main className={s.main}>{children}</main>
-      <div className={s.footer}>Foo</div>
+      <main>{children}</main>
+      <div>Foo</div>
     </div>
   );
 };
