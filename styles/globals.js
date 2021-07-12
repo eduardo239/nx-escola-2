09b13b362cd0db3d483115451e04364ffd853db3
eduardo239 <eucrieiumaconta@gmail.com>
2021-07-12 10:45:19 -0300

    Keep calm and commit

--- styles/globals.js
+++ styles/globals.js
@@ -1,12 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  *,
-  *::after,
-  *::before {
-    box-sizing: border-box;
-  }
-
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
