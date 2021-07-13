4289cca5e5287aea3de13eeac0e05802fbb29c8d
eduardo239 <eucrieiumaconta@gmail.com>
2021-07-13 18:35:07 -0300

    ajustes gerais

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,10 +1,21 @@
 import { supabase } from '../../utils/supabase';
+import { app_name } from '../../utils/constants';
 import Grades from '../../components/table/user/Grades';
+import Head from 'next/head';
 
 const Profile = ({ profile, user_grades }) => {
   if (profile)
     return (
       <section>
+        <Head>
+          <title>{`${app_name} - ${profile.username}`}</title>
+          <meta
+            name="description"
+            content="App Escola - App para acompanhamento estudantil."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
         {profile.username}
         <h1>User</h1>
         {user_grades && <Grades user_grades={user_grades}></Grades>}
@@ -40,7 +51,7 @@ export async function getStaticProps(context) {
 
   let { data: user_grades, error: error_grades } = await supabase
     .from('user_grades')
-    .select('*');
+    .select('*, subject_id(id, name, course_id(id, name, poster))');
 
   if (error_grades) throw new Error(error_grades);
   return {
