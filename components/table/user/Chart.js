9440fb28a45dbe922001324d41fc8370b3fa08dd
eduardo239 <eucrieiumaconta@gmail.com>
2021-07-16 16:51:38 -0300

    adicionada opção de ver as últimas notas

--- components/table/user/Chart.js
+++ components/table/user/Chart.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { formatDateShort } from '../../../utils';
+import { Input } from '../../ui/Form';
 
 const options = {
   scales: {
@@ -15,17 +16,30 @@ const options = {
 };
 
 const Chart = ({ user_grades }) => {
+  const [length, setLength] = useState(5);
+
   const mapDates = () => {
-    return user_grades.map((x) => formatDateShort(x.updated_at));
+    return user_grades.map((x) => formatDateShort(x.updated_at)).slice(-length);
   };
   const mapGrades = () => {
-    return user_grades.map((x) => x.result);
+    return user_grades.map((x) => x.result).slice(-length);
   };
 
   mapDates();
   return (
     <section>
       <h1>Notas</h1>
+
+      <Input
+        type="number"
+        placeholder="Mostrar #n últimas notas .."
+        label="Runtime"
+        id="add-course-runtime"
+        value={length}
+        onChange={(e) => setLength(e.target.value)}
+        className="w-100"
+      />
+
       <Line
         data={{
           labels: mapDates(),
